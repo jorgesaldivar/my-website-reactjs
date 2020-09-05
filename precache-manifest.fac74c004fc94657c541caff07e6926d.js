19c08c5b86424c944377b3bcd80944d1d475d1a0
Jorge Saldivar <1724724+jorgesaldivar@users.noreply.github.com>
2020-09-05 00:00:35 -0500

    Updates

---
+++ precache-manifest.fac74c004fc94657c541caff07e6926d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7bb27896646f02ac2814076fc10a3da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba716fcd465cc2978ed4",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "7aa9ede8718bbe7f94d4",
+    "url": "/static/css/main.de11a86b.chunk.css"
+  },
+  {
+    "revision": "ba716fcd465cc2978ed4",
+    "url": "/static/js/2.9dd0b2fa.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.9dd0b2fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7aa9ede8718bbe7f94d4",
+    "url": "/static/js/main.fc8caff9.chunk.js"
+  },
+  {
+    "revision": "612b057bb57f39025556",
+    "url": "/static/js/runtime-main.bec68f6a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-website-reactjs/precache-manifest.7097e0265b86ce8e08230dac64cfb4d7.js"
+  "/precache-manifest.fac74c004fc94657c541caff07e6926d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-website-reactjs/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
