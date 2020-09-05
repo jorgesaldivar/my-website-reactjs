11dce601072456d1a2a9145cc2dba2c02b6bbc5e
Jorge Saldivar <1724724+jorgesaldivar@users.noreply.github.com>
2020-09-04 21:11:52 -0500

    Updates

---
+++ precache-manifest.c66ea04825fffe17d6e510c6ea22f8ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b816ebdf9e97e0525d5f15b992ce6e5",
+    "url": "/my-website-reactjs/index.html"
+  },
+  {
+    "revision": "ba716fcd465cc2978ed4",
+    "url": "/my-website-reactjs/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "7452e7f2c805f99ba549",
+    "url": "/my-website-reactjs/static/css/main.94438494.chunk.css"
+  },
+  {
+    "revision": "ba716fcd465cc2978ed4",
+    "url": "/my-website-reactjs/static/js/2.9dd0b2fa.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/my-website-reactjs/static/js/2.9dd0b2fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7452e7f2c805f99ba549",
+    "url": "/my-website-reactjs/static/js/main.4a6719c7.chunk.js"
+  },
+  {
+    "revision": "e382ed5f5a4980ce6422",
+    "url": "/my-website-reactjs/static/js/runtime-main.78b3d5c5.js"
+  }
+]);
